af68277b62e79e875c17c91b99a30468594a2c2a
Joseph-Pena <penajoseph270@gmail.com>
2026-03-23 04:08:32 -0400

    Dark Echoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,54 @@
+import { useState } from "react";
+import { episodeList } from "./data.js";
+
 export default function App() {
-  // TODO
+  const [episodes] = useState(episodeList)
+  const [selectedEpisode, setSelectedEpisode] = useState(null)
+
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episodes Details</h2>
+          <p>Select a episode to learn more</p>
+        </section>
+      )
+    } else { 
+      return(
+        <section className="details">
+          <h1>Episode {selectedEpisode.id}</h1>
+          <h2>{selectedEpisode.title}</h2>
+          <p>{selectedEpisode.description}</p>
+          <button>Watch now</button>
+        </section>
+      )
+    };
+  }
+
+  function EpisodeList() {
+  return (
+    <section className="episode">
+      <ul className="episode">
+        {episodes.map((episode) => (
+          <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+            {episode.title}
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+  return (
+    <>
+      <header>
+        <h1>Episodes</h1>
+      </header>
+      <main>
+        <EpisodeList />
+        <EpisodeDetails />
+      </main>
+    </>
+  )
+
 }
